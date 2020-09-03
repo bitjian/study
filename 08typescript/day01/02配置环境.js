20c3ed7debd1b2c2910590a89ced63f2c79dc149
zijianzhang <1183048003@qq.com>
2020-09-03 09:43:11 +0800

    ts编译环境

--- 08typescript/day01/02配置环境.js
+++ 08typescript/day01/02配置环境.js
@@ -1,6 +1,6 @@
 // 拥有node环境
 // 通过npm安装typescript,通过tsc来编译typescript
-// 通过tsc来讲typescript文件编译成javascript
+// 通过tsc来将typescript文件编译成javascript
 
 // webpack配置
 // 创建一个目录
@@ -11,6 +11,7 @@
 // 初始化npm npm init
 // 在项目中安装typescript npm -i typescript
 // 初始化tsconfig.json文件，通过tsc --init 初始化typescript配置文件,在lib可以加载额外的模块
+// "lib": ["es6", "dom"],
 // 安装并配置tslint来约束代码 npm i tslint -g; tslint -i
 
 // 开始webpack配置
@@ -19,6 +20,18 @@
 // "build": "cross-env NODE_ENV=development webpack-dev-server --mode=development --config build/webpack.config.js"
 // 安装 cross-env 判断当前是开发环境还是生产环境
 // 安装 ts-loader 来解析 .ts文件
+// rules: [
+//   {
+//     test: /\.tsx?$/,
+//     use: "ts-loader",
+//     exclude: /node_modules/
+//   }
+// ]
 // 安装 html-webpack-plugin 来配置入口视图
+// plugins: [
+//   new HtmlWebpackPlugin({
+//     template: "./index.html"
+//   })
+// ]
 // 在build文件夹中新建webpack.config.js 来配置webpack
 // 在src文件家中新建main.ts 来编写代码
