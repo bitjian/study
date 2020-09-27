4f4ffb986a0d47d91a7c302726915ed6897bb794
zijianzhang <1183048003@qq.com>
2020-09-27 18:46:39 +0800

    微任务了解

--- 00test/taskRun.js
+++ 00test/taskRun.js
@@ -12,47 +12,47 @@
 // })
 // console.log(5)
 
-process.nextTick(function() {           // 微任务1-1 [2 10 1]
-  console.log('1');                   
-})
+// process.nextTick(function() {           // 微任务1-1 [2 10 1]
+//   console.log('1');                   
+// })
 
-new Promise(function (resolve) {      // [2]
-  console.log('2');                   
-  resolve();                          
-}).then(function () {             // 微任务1-2  [2 10 1 3]         
-  console.log('3');                   
-  setTimeout(function () {      // 宏任务（2-1） [2 10 1 3 5 6 4]
-    console.log('4')
-  });
-});
+// new Promise(function (resolve) {      // [2]
+//   console.log('2');                   
+//   resolve();                          
+// }).then(function () {             // 微任务1-2  [2 10 1 3]         
+//   console.log('3');                   
+//   setTimeout(function () {      // 宏任务（2-1） [2 10 1 3 5 6 4]
+//     console.log('4')
+//   });
+// });
 
-setTimeout(function () {          // 宏任务 1-1    [2 10 1 3 5]
-  console.log('5')                    
-});
+// setTimeout(function () {          // 宏任务 1-1    [2 10 1 3 5]
+//   console.log('5')                    
+// });
 
-new Promise(function (resolve) { // 宏任务 1-2  [2 10 1 3 5 6]
-  setTimeout(function () {            
-    console.log('6')                  
-  });
-  resolve()
-}).then(function () {             // 微任务 1-3       
-  setTimeout(function () {            // 宏任务 2-2  [ 2 10 1 3 5 6 4 7]
-    console.log('7')
-    new Promise(function (resolve) {
-      console.log('12')
-      setTimeout(function () {  // 宏任务 3-1 [ 2 10 1 3 5 6 4 7 8]
-        console.log('8')
-      });
-      resolve()
-    }).then(function () {
-      console.log('11')
-      setTimeout(function () { // 宏任务 4 - 1 [ 2 10 1 3 5 6 4 7 8 9]
-        console.log('9')
-      });
-    });
-  });
-});
-console.log('10')         // [2 10]
+// new Promise(function (resolve) { // 宏任务 1-2  [2 10 1 3 5 6]
+//   setTimeout(function () {            
+//     console.log('6')                  
+//   });
+//   resolve()
+// }).then(function () {             // 微任务 1-3       
+//   setTimeout(function () {            // 宏任务 2-2  [ 2 10 1 3 5 6 4 7]
+//     console.log('7')
+//     new Promise(function (resolve) {
+//       console.log('12')
+//       setTimeout(function () {  // 宏任务 3-1 [ 2 10 1 3 5 6 4 7 8]
+//         console.log('8')
+//       });
+//       resolve()
+//     }).then(function () {
+//       console.log('11')
+//       setTimeout(function () { // 宏任务 4 - 1 [ 2 10 1 3 5 6 4 7 8 9]
+//         console.log('9')
+//       });
+//     });
+//   });
+// });
+// console.log('10')         // [2 10]
 
 // const fun1 = (cb) => {
 //   cb()
@@ -89,3 +89,29 @@ console.log('10')         // [2 10]
 // })
 
 // console.log('开始222');
+
+let consoleLogA = function () {
+  consoleLogB()
+  console.log('a')
+}
+let consoleLogC = function () {
+  console.log('c')
+}
+let consoleLogD = function () {
+  console.log('d')
+}
+let consoleLogB = function () {
+  new Promise((resolve, reject) => {
+    console.log('b')
+    resolve()
+  }).then(() => {
+    consoleLogC()
+    // setTimeout(() => {
+    //   consoleLogC()
+    // }, 3000);
+  })
+  consoleLogD()
+  return ''
+}
+
+consoleLogA()
