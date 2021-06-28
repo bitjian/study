7af55dc1f1de43b3672d97f6fd50b6418dc84662
zijianzhang <1183048003@qq.com>
2021-06-28 19:22:49 +0800

    复习节流防抖

--- 13js基础66条/055防抖函数/demo.js
+++ 13js基础66条/055防抖函数/demo.js
@@ -1,12 +1,12 @@
 // 防抖函数，在某一时间范围内，如果继续触发了这个方法，则重新计时,直到没有触发执行
 // 防抖函数是 取消前面的操作，重新计时，在时间结束后， 执行最后一个函数，节流是在时间范围内执行第一个函数，阻止后面的操作
-let timer = null
 function debounce(func, time) {
+  let timer = null
   return function() {
     let context = this
     let arg = arguments
     if(timer) {
-      clearInterval(timer)
+      clearTimeout(timer)
     }
     timer = setTimeout(() => {
       func.apply(context, arg)
@@ -14,9 +14,11 @@ function debounce(func, time) {
   }
 }
 
-let d1 = debounce((message) => {console.log(message),2000})
+let d1 = debounce((message) => {console.log(message),3000})
 d1(1)
 d1(2)
+d1(2.1)
+d1(2.3)
 setTimeout(() => {
   d1(3)
 }, 1000);
