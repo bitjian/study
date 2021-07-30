23a6cbaff0662cce72bca02b25d1e1f591383c16
zijianzhang <1183048003@qq.com>
2021-07-30 14:42:58 +0800

    vue通过$attrs和$listeners透传属性和方法给子组件

---
+++ 09vue高级/02$attrs和$listeners/MyButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-button v-bind="customAttrs" v-on="$listeners"></el-button>
+</template>
+
+<script>
+export default {
+  name: 'MyButton',
+  inheritAttrs:false, // 让没有在props里什么的属性，也可以绑定到子元素上，不然绑定的是子元素的根元素上
+  props: {
+
+  },
+  computed: {
+    customAttrs() {
+      return {
+        size: 'mini',
+        ...this.$attrs
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
